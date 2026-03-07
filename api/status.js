159dd3213892e63f24200fc607d1ba8de994c6d7
VXP..cvxpk <124467084+AC447724@users.noreply.github.com>
2026-03-06 23:21:22 -0500

    Update status.js

--- api/status.js
+++ api/status.js
@@ -14,6 +14,6 @@ export default async function handler(req, res) {
     res.setHeader('Content-Type', 'application/json');
     return res.status(200).json(botData);
   } catch (error) {
-    return res.status(500).json({ error: error.message });
+    return res.status(500).json({ error: "Database Connection Error" });
   }
 }
